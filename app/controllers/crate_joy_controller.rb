dea5650797655a444cb4e33ae5ac705f7efaa703
David <agile_check_in@Davids-MacBook-Pro.local>
2016-03-16 12:08:45 -0500

    [#00] Don't render anything, it's fine!

--- app/controllers/crate_joy_controller.rb
+++ app/controllers/crate_joy_controller.rb
@@ -2,5 +2,6 @@ class CrateJoyController < ApplicationController
   skip_before_filter :verify_authenticity_token
   def new
     ParamsForDaveMailer.test_email(params)
+    render nothing: true
   end
 end
