ab64201243cbc8b9a69cc22bbd7457b04bca4591
David <agile_check_in@Davids-MacBook-Pro.local>
2016-03-16 12:13:23 -0500

    [#00] Actually send the email!

--- app/controllers/crate_joy_controller.rb
+++ app/controllers/crate_joy_controller.rb
@@ -1,7 +1,7 @@
 class CrateJoyController < ApplicationController
   skip_before_filter :verify_authenticity_token
   def new
-    ParamsForDaveMailer.test_email(params)
+    ParamsForDaveMailer.test_email(params).deliver_now
     render nothing: true
   end
 end
